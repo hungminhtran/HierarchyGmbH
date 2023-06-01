91f3880272ad3143094150a33e5c4f2827c66a39
Tran Minh-Hung <minhhung.1301@gmail.com>
2023-06-01 21:00:34 +0700

    HierarchyGmbH: init commit.

---
+++ src/test/java/com/hierarchy/gmbh/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hierarchy.gmbh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
